3f98ba5c19638374ca15d1f31101127cf93459ed
Gautier Ducurtil <gaducurt@student.42.fr>
2026-04-29 10:57:36 +0200

    KICK.cpp

--- sources/command/KICK.cpp
+++ sources/command/KICK.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   KICK.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gaducurt <gaducurt@student.42lyon.fr>      +#+  +:+       +#+        */
+/*   By: gaducurt <gaducurt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/27 10:55:25 by gaducurt          #+#    #+#             */
-/*   Updated: 2026/04/27 10:55:28 by gaducurt         ###   ########lyon.fr   */
+/*   Updated: 2026/04/29 10:55:57 by gaducurt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,4 +15,6 @@
 #include "Channel.hpp"
 
 void Server::KICK(std::string const&line, )
-
+{
+    
+}
